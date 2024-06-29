af93e9147c4da311e2bb16d2384056f4d89c0f48
coolnerdycoder <156668071+coolnerdycoder@users.noreply.github.com>
2024-06-29 19:35:10 +0545

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, jsonify
-from flask_cors import CORS  # Import CORS from flask_cors module
 import json
-from collections import OrderedDict
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all origins
 
 # Load keys from keys.json file
 def load_keys():
@@ -16,20 +13,7 @@ def get_keys_by_channel_id(channel_id):
     keys_data = load_keys()
     for item in keys_data:
         if item['channel_id'] == channel_id:
-            try:
-                # Transform keys data into desired format
-                transformed_keys = {
-                    "keys": [OrderedDict([
-                        ("kty", key["kty"]),
-                        ("k", key["k"]),
-                        ("kid", key["kid"])
-                    ]) for key in item["keys"]],
-                    "type": "temporary"
-                }
-                return jsonify(transformed_keys)
-            except KeyError as e:
-                return jsonify({"error": f"Invalid keys format in keys.json: Missing key field ({str(e)})"}), 500  # Server error
-
+            return jsonify(item['keys'])
     return jsonify({"error": "Keys not found for this channel_id"}), 404
 
 if __name__ == '__main__':
